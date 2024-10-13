e2eb49160ec8f1ede7d4b0b1d723d056a558e613
Alex <30626291+ipruning@users.noreply.github.com>
2024-10-13 22:43:31 +0800

    feat: add data validation and conversion with Patito and Polars
    - Add a new Python file defining a `Product` class using Patito and validate a Polars DataFrame against it
    - Add a new Python file defining an `Item` class using pydantic and convert a Polars DataFrame into a list of `Item` instances
    - Update dependencies in `pyproject.toml` with new versions and add `patito` and `polars` as dependencies
    
    Signed-off-by: Alex <30626291+ipruning@users.noreply.github.com>

---
+++ 1.py
@@ -0,0 +1,25 @@
+from typing import Literal
+
+import patito as pt
+import polars as pl
+
+
+class Product(pt.Model):
+    product_id: int = pt.Field(unique=True)
+    name: str
+    temperature_zone: Literal["dry", "cold", "frozen"]
+    demand_percentage: float = pt.Field(constraints=pt.field.sum() == 100.0)
+
+
+invalid_product_df = pl.DataFrame(
+    {
+        "product_id": [64, 64],
+        "name": ["Pizza", "Cereal"],
+        "temperature_zone": ["oven", "dry"],
+        "demand_percentage": [0.07, 0.16],
+    }
+)
+
+print(invalid_product_df)
+
+Product.validate(invalid_product_df)
